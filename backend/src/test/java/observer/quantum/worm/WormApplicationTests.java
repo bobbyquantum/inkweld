071b1756f4e56c3b7e366937b511d083b4310276
BobbyQuantum <166758526+BobbyQuantum@users.noreply.github.com>
2024-08-05 21:55:21 +0100

    chore: Add testcontainers

--- backend/src/test/java/observer/quantum/worm/WormApplicationTests.java
+++ backend/src/test/java/observer/quantum/worm/WormApplicationTests.java
@@ -3,12 +3,22 @@ package observer.quantum.worm;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Slf4j
-@SpringBootTest()
+@Testcontainers
+@SpringBootTest(properties = "application-test.yml")
 @ActiveProfiles("test")
 class WormApplicationTests {
+
+    @SuppressWarnings("unused")
+    @Container
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
 
     @Test
     void contextLoads() {
