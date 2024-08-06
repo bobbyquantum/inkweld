427c1ef61d6357af2f9a14d3cddc7d6d8b5e32eb
BobbyQuantum <166758526+BobbyQuantum@users.noreply.github.com>
2024-08-06 18:01:03 +0100

    chore: Add .env

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,23 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'worm-frontend';
   loggedin = "checking";
+  user: any = {}
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get('/api/users/me').subscribe(response => {
       console.log(response);
       this.loggedin="yes";
+      this.user = response;
     }, error => {
       console.error(error);
       if (error.status==401) {
