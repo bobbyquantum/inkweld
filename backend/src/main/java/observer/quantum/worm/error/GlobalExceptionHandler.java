aaf87c98f358708a8bee3ddadc959ca93c2b5fe8
Bobby Quantum <bobby@quantum.observer>
2024-09-27 23:39:53 +0100

    Move some config

--- backend/src/main/java/observer/quantum/worm/error/GlobalExceptionHandler.java
+++ backend/src/main/java/observer/quantum/worm/error/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@SuppressWarnings("unused")
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
