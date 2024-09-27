a0f01c7c4ce50da27ec696f76f46ad440322f594
Bobby Quantum <bobby@quantum.observer>
2024-09-27 20:57:11 +0100

    Separate out csrf token handler

--- backend/src/main/java/observer/quantum/worm/global/SecurityConfig.java
+++ backend/src/main/java/observer/quantum/worm/global/SecurityConfig.java
@@ -1,8 +1,6 @@
 package observer.quantum.worm.global;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.function.Supplier;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,10 +12,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.*;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
-import org.springframework.util.StringUtils;
 
 @Slf4j
-@SuppressWarnings("unused")
 @Profile("default")
 @Configuration
 @EnableWebSecurity
@@ -79,37 +75,5 @@ public class SecurityConfig {
   @Bean
   public StrictHttpFirewall httpFirewall() {
     return new StrictHttpFirewall();
-  }
-}
-
-final class SpaCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler {
-  private final CsrfTokenRequestHandler delegate = new XorCsrfTokenRequestAttributeHandler();
-
-  @Override
-  public void handle(
-      HttpServletRequest request, HttpServletResponse response, Supplier<CsrfToken> csrfToken) {
-    /*
-     * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection of the CsrfToken
-     * when it is rendered in the response body.
-     */
-    this.delegate.handle(request, response, csrfToken);
-  }
-
-  @Override
-  public String resolveCsrfTokenValue(HttpServletRequest request, CsrfToken csrfToken) {
-    /*
-     * If the request contains a request header, use CsrfTokenRequestAttributeHandler to resolve the
-     * CsrfToken. This applies when a single-page application includes the header value
-     * automatically, which was obtained via a cookie containing the raw CsrfToken.
-     */
-    if (StringUtils.hasText(request.getHeader(csrfToken.getHeaderName()))) {
-      return super.resolveCsrfTokenValue(request, csrfToken);
-    }
-    /*
-     * In all other cases (e.g. if the request contains a request parameter), use
-     * XorCsrfTokenRequestAttributeHandler to resolve the CsrfToken. This applies when a server-side
-     * rendered form includes the _csrf request parameter as a hidden input.
-     */
-    return this.delegate.resolveCsrfTokenValue(request, csrfToken);
   }
 }
