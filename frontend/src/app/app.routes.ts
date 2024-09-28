ffede06059b1da7cf69012339ef532050e5653ca
Bobby Quantum <bobby@quantum.observer>
2024-09-28 22:35:22 +0100

    start on register page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NewProjectComponent } from './pages/new-project/new-project.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register',
   },
   {
     path: 'new-project',
