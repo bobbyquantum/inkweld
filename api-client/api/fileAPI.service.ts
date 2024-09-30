329901e2958a2d167a0fc2e78b221937a3aa97a1
Bobby Quantum <bobby@quantum.observer>
2024-10-01 00:19:14 +0100

    More progress

--- api-client/api/fileAPI.service.ts
+++ api-client/api/fileAPI.service.ts
@@ -37,7 +37,7 @@ import { Configuration }                                     from '../configurat
 })
 export class FileAPIService {
 
-    protected basePath = 'http://localhost:58977';
+    protected basePath = 'http://localhost:61521';
     public defaultHeaders = new HttpHeaders();
     public configuration = new Configuration();
     public encoder: HttpParameterCodec;
@@ -257,10 +257,10 @@ export class FileAPIService {
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
      * @param reportProgress flag to report request and response progress.
      */
-    public getFile(fileId: string, range?: string, download?: boolean, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/octet-stream' | 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<any>;
-    public getFile(fileId: string, range?: string, download?: boolean, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/octet-stream' | 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<HttpResponse<any>>;
-    public getFile(fileId: string, range?: string, download?: boolean, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/octet-stream' | 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<HttpEvent<any>>;
-    public getFile(fileId: string, range?: string, download?: boolean, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/octet-stream' | 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<any> {
+    public getFile(fileId: string, range?: string, download?: boolean, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'application/octet-stream', context?: HttpContext, transferCache?: boolean}): Observable<any>;
+    public getFile(fileId: string, range?: string, download?: boolean, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'application/octet-stream', context?: HttpContext, transferCache?: boolean}): Observable<HttpResponse<any>>;
+    public getFile(fileId: string, range?: string, download?: boolean, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'application/octet-stream', context?: HttpContext, transferCache?: boolean}): Observable<HttpEvent<any>>;
+    public getFile(fileId: string, range?: string, download?: boolean, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json' | 'application/octet-stream', context?: HttpContext, transferCache?: boolean}): Observable<any> {
         if (fileId === null || fileId === undefined) {
             throw new Error('Required parameter fileId was null or undefined when calling getFile.');
         }
@@ -280,8 +280,8 @@ export class FileAPIService {
         if (localVarHttpHeaderAcceptSelected === undefined) {
             // to determine the Accept header
             const httpHeaderAccepts: string[] = [
-                'application/octet-stream',
-                'application/json'
+                'application/json',
+                'application/octet-stream'
             ];
             localVarHttpHeaderAcceptSelected = this.configuration.selectHeaderAccept(httpHeaderAccepts);
         }
--- backend/src/main/java/observer/quantum/worm/user/UsernameAvailabilityResponse.java
+++ backend/src/main/java/observer/quantum/worm/user/UsernameAvailabilityResponse.java
@@ -3,27 +3,27 @@ package observer.quantum.worm.user;
 import java.util.List;
 
 public class UsernameAvailabilityResponse {
-    private boolean available;
-    private List<String> suggestions;
+  private boolean available;
+  private List<String> suggestions;
 
-    public UsernameAvailabilityResponse(boolean available, List<String> suggestions) {
-        this.available = available;
-        this.suggestions = suggestions;
-    }
+  public UsernameAvailabilityResponse(boolean available, List<String> suggestions) {
+    this.available = available;
+    this.suggestions = suggestions;
+  }
 
-    public boolean isAvailable() {
-        return available;
-    }
+  public boolean isAvailable() {
+    return available;
+  }
 
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
+  public void setAvailable(boolean available) {
+    this.available = available;
+  }
 
-    public List<String> getSuggestions() {
-        return suggestions;
-    }
+  public List<String> getSuggestions() {
+    return suggestions;
+  }
 
-    public void setSuggestions(List<String> suggestions) {
-        this.suggestions = suggestions;
-    }
+  public void setSuggestions(List<String> suggestions) {
+    this.suggestions = suggestions;
+  }
 }
