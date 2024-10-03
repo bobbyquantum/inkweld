3c342d47938d92668fe6348026722ab83cf117bb
Bobby Quantum <bobby@quantum.observer>
2024-10-03 23:29:49 +0100

    Storybook fixes

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,5 +1,4 @@
-import { StorybookConfig } from '@storybook/angular';
-import path from 'path';
+import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -7,50 +6,11 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    'storybook-dark-mode',
   ],
   framework: {
     name: '@storybook/angular',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
-  staticDirs: [],
-  webpackFinal: async config => {
-    if (config.module && config.module.rules) {
-      // Safely filter out existing SCSS and CSS rules
-      config.module.rules = config.module.rules.filter(
-        (rule): rule is webpack.RuleSetRule => {
-          return (
-            typeof rule !== 'string' &&
-            rule !== false &&
-            rule !== null &&
-            rule !== undefined &&
-            !(
-              'test' in rule &&
-              rule.test instanceof RegExp &&
-              (rule.test.test('.scss') || rule.test.test('.css'))
-            )
-          );
-        }
-      );
-
-      // Add rule for SCSS files in the 'src' directory
-      config.module.rules.push({
-        test: /\.scss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, '../src'),
-      });
-
-      // Add rule for CSS files in the 'src' directory
-      config.module.rules.push({
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-        include: path.resolve(__dirname, '../src'),
-      });
-    }
-    return config;
-  },
 };
-
 export default config;
