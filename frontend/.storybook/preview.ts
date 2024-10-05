ed1de09b4acbf39c67d8b037b7fc8e4b89f566eb
Bobby Quantum <bobby@quantum.observer>
2024-10-05 11:58:07 +0100

    Reorganise

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,7 +1,9 @@
-import { applicationConfig, type Preview } from '@storybook/angular';
 import { setCompodocJson } from '@storybook/addon-docs/angular';
 import docJson from '../documentation.json';
+import { applicationConfig, type Preview } from '@storybook/angular';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { themes } from '@storybook/theming';
+
 setCompodocJson(docJson);
 
 const preview: Preview = {
@@ -11,10 +13,13 @@ const preview: Preview = {
       lightClass: 'light-theme',
       stylePreview: true,
     },
+    // docs: {
+    //   theme: themes.dark,
+    // },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
     },
   },
@@ -26,65 +31,3 @@ const preview: Preview = {
 };
 
 export default preview;
-
-// import { setCompodocJson } from '@storybook/addon-docs/angular';
-// import docJson from '../documentation.json';
-// import { ThemeOption } from '../src/themes/theme.service';
-// import {
-//   moduleMetadata,
-//   applicationConfig,
-//   type Preview,
-// } from '@storybook/angular';
-// import { provideAnimations } from '@angular/platform-browser/animations';
-
-// //import '../src/themes/theme.scss';
-
-// setCompodocJson(docJson);
-
-// const applyTheme = (theme: ThemeOption) => {
-//   const body = document.body;
-//   body.classList.remove('light-theme', 'dark-theme');
-//   body.classList.add(theme);
-// };
-
-// const preview: Preview = {
-//   parameters: {
-//     actions: { argTypesRegex: '^on[A-Z].*' },
-//     controls: {
-//       matchers: {
-//         color: /(background|color)$/i,
-//         date: /Date$/,
-//       },
-//     },
-//   },
-//   decorators: [
-//     moduleMetadata({
-//       imports: [],
-//     }),
-//     applicationConfig({
-//       providers: [provideAnimations()],
-//     }),
-//     (Story, context) => {
-//       const theme = context.globals['theme'] as ThemeOption;
-//       applyTheme(theme);
-//       return Story();
-//     },
-//   ],
-//   globalTypes: {
-//     theme: {
-//       name: 'Theme',
-//       description: 'Global theme for components',
-//       defaultValue: 'dark-theme' as ThemeOption,
-//       toolbar: {
-//         icon: 'circlehollow',
-//         items: [
-//           { value: 'light-theme', icon: 'circlehollow', title: 'Light' },
-//           { value: 'dark-theme', icon: 'circle', title: 'Dark' },
-//         ],
-//         showName: true,
-//       },
-//     },
-//   },
-// };
-
-// export default preview;
