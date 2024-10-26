e5059317b06657c6c802959a87e1a0d1fcf6b206
Bobby Quantum <bobby@quantum.observer>
2024-10-26 22:05:52 +0100

    Update api client

--- frontend/src/app/components/project-main-menu/project-main-menu.component.spec.ts
+++ frontend/src/app/components/project-main-menu/project-main-menu.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectMainMenuComponent } from './project-main-menu.component';
+import { HttpClient } from '@angular/common/http';
 
 describe('ProjectMainMenuComponent', () => {
   let component: ProjectMainMenuComponent;
   let fixture: ComponentFixture<ProjectMainMenuComponent>;
+  let httpClientMock: jasmine.SpyObj<HttpClient>;
 
   beforeEach(async () => {
+    httpClientMock = jasmine.createSpyObj('HttpClient', [
+      'get',
+      'post',
+      'put',
+      'delete',
+    ]);
     await TestBed.configureTestingModule({
       imports: [ProjectMainMenuComponent],
+      providers: [{ provide: HttpClient, useValue: httpClientMock }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProjectMainMenuComponent);
