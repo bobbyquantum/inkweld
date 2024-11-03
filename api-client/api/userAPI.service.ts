4e32b84fcaef62faced9999737b2f856adbac9d6
Bobby Quantum <bobby@quantum.observer>
2024-11-03 23:34:48 +0000

    Add bulk position update

--- api-client/api/userAPI.service.ts
+++ api-client/api/userAPI.service.ts
@@ -41,7 +41,7 @@ import { Configuration }                                     from '../configurat
 })
 export class UserAPIService {
 
-    protected basePath = 'http://localhost:58068';
+    protected basePath = 'http://localhost:60152';
     public defaultHeaders = new HttpHeaders();
     public configuration = new Configuration();
     public encoder: HttpParameterCodec;
