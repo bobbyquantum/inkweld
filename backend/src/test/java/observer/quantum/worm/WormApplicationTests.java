3a570b2dc291c71f44a86fa6c8bb4d280d5bd678
Bobby Quantum <bobby@quantum.observer>
2024-11-16 19:24:38 +0000

    Remove context tests for CI speed

--- backend/src/test/java/observer/quantum/worm/WormApplicationTests.java
+++ backend/src/test/java/observer/quantum/worm/WormApplicationTests.java
@@ -1,33 +1,35 @@
-package observer.quantum.worm;
+// package observer.quantum.worm;
 
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+// import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@Testcontainers
-@SpringBootTest
-@ActiveProfiles("test")
-class WormApplicationTests {
+// import org.junit.jupiter.api.Disabled;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.ActiveProfiles;
+// import org.springframework.test.context.DynamicPropertyRegistry;
+// import org.springframework.test.context.DynamicPropertySource;
+// import org.testcontainers.containers.PostgreSQLContainer;
+// import org.testcontainers.junit.jupiter.Container;
+// import org.testcontainers.junit.jupiter.Testcontainers;
+// @Disabled
+// @Slf4j
+// @Testcontainers
+// @SpringBootTest
+// @ActiveProfiles("test")
+// class WormApplicationTests {
 
-    @Container
-    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17");
+//     @Container
+//     static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17");
 
-    @DynamicPropertySource
-    static void registerProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresContainer::getUsername);
-        registry.add("spring.datasource.password", postgresContainer::getPassword);
-    }
+//     @DynamicPropertySource
+//     static void registerProperties(DynamicPropertyRegistry registry) {
+//         registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+//         registry.add("spring.datasource.username", postgresContainer::getUsername);
+//         registry.add("spring.datasource.password", postgresContainer::getPassword);
+//     }
 
-    @Test
-    void contextLoads() {
-        log.info("Context loads completed");
-    }
-}
+//     @Test
+//     void contextLoads() {
+//         log.info("Context loads completed");
+//     }
+// }
