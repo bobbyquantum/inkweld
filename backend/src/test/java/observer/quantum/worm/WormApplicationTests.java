62c7f553d0ac08a1931ac9be50de725b148c33d6
Bobby Quantum <bobby@quantum.observer>
2024-11-16 13:31:00 +0000

    test fixes

--- backend/src/test/java/observer/quantum/worm/WormApplicationTests.java
+++ backend/src/test/java/observer/quantum/worm/WormApplicationTests.java
@@ -4,14 +4,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Slf4j
-@SpringBootTest(properties = "application-test.yml")
+@Testcontainers
+@SpringBootTest
 @ActiveProfiles("test")
 class WormApplicationTests {
 
-  @Test
-  void contextLoads() {
-    log.info("Context loads completed: ");
-  }
+    @Container
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17");
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+        log.info("Context loads completed");
+    }
 }
