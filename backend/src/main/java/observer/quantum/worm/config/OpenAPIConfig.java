cc9db491b8cf77f0093e5258b8805b74c7353063
Bobby Quantum <bobby@quantum.observer>
2024-11-17 21:44:19 +0000

    Remove some unneeded comments

--- backend/src/main/java/observer/quantum/worm/config/OpenAPIConfig.java
+++ backend/src/main/java/observer/quantum/worm/config/OpenAPIConfig.java
@@ -22,18 +22,7 @@ public class OpenAPIConfig {
                 .description(
                     "Worm tunnel protocol - Secure API for managing projects and user data")
                 .version(version)
-            //                        .contact(new Contact()
-            //                                .name("API Support")
-            //                                .email("support@wormapi.com")
-            //                                .url("https://www.wormapi.com/support"))
-            //                        .license(new License()
-            //                                .name("Apache 2.0")
-            //
-            // .url("http://www.apache.org/licenses/LICENSE-2.0.html"))
             )
-        //                .externalDocs(new ExternalDocumentation()
-        //                        .description("Worm API Documentation")
-        //                        .url("https://www.wormapi.com/docs"))
         .tags(
             Arrays.asList(
                 new Tag()
@@ -48,18 +37,7 @@ public class OpenAPIConfig {
                                new
         Server().url("http://localhost:8333/").description("Development server")
                        ))
-        //                .security(Arrays.asList(new SecurityRequirement().addList("OAuth2")))
-        //                .components(new Components()
-        //                        .addSecuritySchemes("OAuth2", new SecurityScheme()
-        //                                .type(SecurityScheme.Type.OAUTH2)
-        //                                .flows(new OAuthFlows()
-        //                                        .implicit(new OAuthFlow()
-        //
-        // .authorizationUrl("/oauth2/authorization/github")
-        //                                        )
-        //                                )
-        //                        )
-        //                )
+
         .specVersion(SpecVersion.V31);
   }
 }
