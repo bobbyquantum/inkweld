597577cc14cfc33ebe36ab1dc308091dbc163d59
Bobby Quantum <bobby@quantum.observer>
2024-12-12 23:37:45 +0000

    Remove broken tests add add some working ones

---
+++ bridge/jest.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+};
+
+export default config;
