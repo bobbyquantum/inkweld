9f96e534b04f893407dc875660de4f38e4f2aee3
Bobby Quantum <bobby@quantum.observer>
2025-01-19 12:42:54 +0000

    chore: refactor jest configuration for TypeScript and coverage settings

--- worm-server/jest.config.cjs
+++ worm-server/jest.config.cjs
@@ -9,19 +9,6 @@ module.exports = {
   // Let Jest know which file extensions to treat as ESM
   extensionsToTreatAsEsm: ['.ts'],
   transform: {},
-  // transform: {
-  //   // Transform TS and JS files via ts-jest, with ESM turned on
-  //   '^.+\\.(t|j)s$': [
-  //     'ts-jest',
-  //     {
-  //       useESM: true,
-  //     },
-  //   ],
-  // },
-
-  // By default, Jest won't transform anything in node_modules.
-  // We NEED to transform `@modelcontextprotocol/sdk` because it's ESM-only.
-  // transformIgnorePatterns: ['node_modules/(?!@modelcontextprotocol/sdk)'],
 
   // Allows .js imports in TS files to be mapped back to .ts
   // (Though you might not strictly need this if everything's .ts)
@@ -30,29 +17,6 @@ module.exports = {
   },
 
   // The rest of your coverage, etc., can live here...
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: 'coverage',
 };
-// "jest": {
-//   "moduleFileExtensions": [
-//     "js",
-//     "json",
-//     "ts"
-//   ],
-//   "rootDir": "src",
-//   "testRegex": ".*\\.spec\\.ts$",
-//   "transform": {
-//     "^.+\\.(t|j)s$": ["ts-jest", {
-//       "useESM": true
-//     }]
-//   },
-//   "extensionsToTreatAsEsm": [".ts"],
-//   "moduleNameMapper": {
-//     "^(\\.{1,2}/.*)\\.js$": "$1"
-//   },
-//   "collectCoverageFrom": [
-//     "**/*.(t|j)s"
-//   ],
-//   "coverageDirectory": "../coverage",
-//   "testEnvironment": "node"
-// },
