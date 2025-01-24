22b4d94c7cf77d62ddbf613b91e773566e45c3cb
Bobby Quantum <bobby@quantum.observer>
2025-01-24 00:11:32 +0000

    chore: Remove fastify due to issues with static files, maybe restore later

--- worm-server/src/app.module.ts
+++ worm-server/src/app.module.ts
@@ -1,8 +1,6 @@
 import {
   Module,
   NestModule,
-  MiddlewareConsumer,
-  RequestMethod,
   Logger,
 } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -10,7 +8,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { UserEntity } from './user/user.entity.js';
 import { AuthModule } from './auth/auth.module.js';
-import { createProxyMiddleware } from 'http-proxy-middleware';
 import { UserSessionEntity } from './auth/session.entity.js';
 import { PassportModule } from '@nestjs/passport';
 import { ProjectModule } from './project/project.module.js';
@@ -19,6 +16,7 @@ import { ProjectElementModule } from './project/element/project-element.module.j
 import { WsModule } from './ws/ws.module.js';
 import { McpModule } from './mcp/mcp.module.js';
 import * as path from 'path';
+import { cwd } from 'process';
 
 @Module({
   imports: [
@@ -32,20 +30,10 @@ import * as path from 'path';
     }),
     ServeStaticModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        const isDev = configService.get('NODE_ENV') !== 'production';
-        if (isDev) {
-          return [];
-        }
+      useFactory: () => {
         return [
           {
-            rootPath: path.resolve('frontend/dist/worm-frontend/browser'),
-            serveRoot: '/',
-            serveStaticOptions: {
-              index: 'index.html',
-              fallthrough: false,
-              decorateReply: true
-            },
+            rootPath: path.resolve(path.join(cwd(),'../frontend/dist/browser')),
           },
         ];
       },
@@ -77,25 +65,5 @@ import * as path from 'path';
 export class AppModule implements NestModule {
   private readonly logger = new Logger(AppModule.name);
 
-  configure(consumer: MiddlewareConsumer) {
-    const configService = new ConfigService();
-    const isDev = configService.get('NODE_ENV') !== 'production';
-
-    if (isDev) {
-      consumer
-        .apply(
-          createProxyMiddleware({
-            target: 'http://localhost:4200',
-            changeOrigin: true,
-            ws: true,
-          }),
-        )
-        .exclude({ path: 'api/*path', method: RequestMethod.ALL })
-        .exclude({ path: 'login/*path', method: RequestMethod.ALL })
-        .exclude({ path: 'oauth2/*path', method: RequestMethod.ALL })
-        .exclude({ path: 'mcp/*path', method: RequestMethod.ALL })
-        .exclude({ path: 'ws/yjs/*path', method: RequestMethod.ALL })
-        .forRoutes({ path: '*', method: RequestMethod.ALL });
-    }
-  }
+  configure() {}
 }
