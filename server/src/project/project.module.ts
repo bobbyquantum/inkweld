e3ed015b7e0e7d60ec857d85e909498e00c66960
Bobby Quantum <bobby@quantum.observer>
2025-03-16 19:33:14 +0000

    refactor(project): remove ProjectElementModule and integrate its components directly into ProjectModule

--- server/src/project/project.module.ts
+++ server/src/project/project.module.ts
@@ -5,16 +5,27 @@ import { ProjectService } from './project.service.js';
 import { ProjectController } from './project.controller.js';
 import { UserEntity } from '../user/user.entity.js';
 import { UserModule } from '../user/user.module.js';
-import { ProjectElementModule } from './element/project-element.module.js';
+import { ProjectElementController } from './element/project-element.controller.js';
+import { ProjectElementService } from './element/project-element.service.js';
+import { ImageStorageService } from './element/image-storage.service.js';
+import { LevelDBManagerService } from '../common/persistence/leveldb-manager.service.js';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ProjectEntity, UserEntity]),
     UserModule,
-    ProjectElementModule,
   ],
-  controllers: [ProjectController],
-  providers: [ProjectService],
-  exports: [ProjectService],
+  controllers: [ProjectController, ProjectElementController],
+  providers: [
+    ProjectService,
+    ProjectElementService,
+    ImageStorageService,
+    LevelDBManagerService,
+  ],
+  exports: [
+    ProjectService,
+    ProjectElementService,
+    ImageStorageService,
+  ],
 })
 export class ProjectModule {}
