403e258c9fc738fa1a7f77a1290b7fd6042ba767
Bobby Quantum <bobby@quantum.observer>
2025-04-23 18:56:56 +0100

    feat: Redesign user profile to be more responsive for mobile screens

--- frontend/src/app/dialogs/edit-avatar-dialog/edit-avatar-dialog.component.ts
+++ frontend/src/app/dialogs/edit-avatar-dialog/edit-avatar-dialog.component.ts
@@ -4,8 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { UserService } from '@services/user.service';
-import { ImageCroppedEvent, ImageCropperComponent } from 'ngx-image-cropper';
+import {
+  ImageCroppedEvent,
+  ImageCropperComponent,
+  LoadedImage,
+} from 'ngx-image-cropper';
 import { firstValueFrom } from 'rxjs';
 
 @Component({
@@ -25,13 +30,19 @@ import { firstValueFrom } from 'rxjs';
 export class EditAvatarDialogComponent {
   protected dialogRef = inject(MatDialogRef<EditAvatarDialogComponent>);
   private userService = inject(UserService);
+  private sanitizer = inject(DomSanitizer);
 
   imageChangedEvent: Event | null = null;
-  croppedImage: string = '';
+  croppedImage: SafeUrl | null = null;
+  croppedBlob: Blob | null = null;
   fileName = '';
   isSubmitting = false;
+  isCropperReady = false;
+  hasImageLoaded = false;
+  hasLoadFailed = false;
 
   fileChangeEvent(event: Event): void {
+    this.resetState();
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       this.imageChangedEvent = event;
@@ -40,17 +51,46 @@ export class EditAvatarDialogComponent {
   }
 
   imageCropped(event: ImageCroppedEvent) {
-    this.croppedImage = event.base64!;
+    if (event.objectUrl && event.blob) {
+      this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(
+        event.objectUrl
+      );
+      this.croppedBlob = event.blob;
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onImageLoaded(image: LoadedImage) {
+    this.hasImageLoaded = true;
+  }
+
+  onCropperReady() {
+    this.isCropperReady = true;
+  }
+
+  onLoadImageFailed() {
+    this.hasLoadFailed = true;
+    alert('Failed to load image. Please try another file.');
+  }
+
+  resetState() {
+    this.imageChangedEvent = null;
+    this.croppedImage = null;
+    this.croppedBlob = null;
+    this.hasImageLoaded = false;
+    this.isCropperReady = false;
+    this.hasLoadFailed = false;
   }
 
   async submit(): Promise<void> {
-    if (!this.croppedImage) {
+    if (!this.croppedBlob || !this.fileName) {
       return;
     }
     this.isSubmitting = true;
     try {
-      const blob = await (await fetch(this.croppedImage)).blob();
-      const file = new File([blob], this.fileName, { type: blob.type });
+      const file = new File([this.croppedBlob], this.fileName, {
+        type: this.croppedBlob.type || 'image/png',
+      });
       await firstValueFrom(this.userService.uploadAvatar(file));
       this.dialogRef.close(true);
     } catch (error) {
