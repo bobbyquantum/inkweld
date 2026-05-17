a12dcfcae066e0e82148086ab8386d090f4ddb09
BobbyQuantum <bobby@quantum.observer>
2026-05-17 09:23:35 +0100

    fix(lint): remove unused beforeAll import

--- frontend/src/app/components/project-tree/project-tree.component.spec.ts
+++ frontend/src/app/components/project-tree/project-tree.component.spec.ts
@@ -19,7 +19,6 @@ import { SettingsService } from '@services/core/settings.service';
 import { ProjectStateService } from '@services/project/project-state.service';
 import {
   afterEach,
-  beforeAll,
   beforeEach,
   describe,
   expect,
